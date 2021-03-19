c385f0e456618d4c1453847f9e2b87ac0f96ae90
milee yu <mileeyu@gmail.com>
2021-03-18 21:48:59 -0700

    updated homepage

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,25 @@
-console.log("Built with Cocobits");
+console.log("Built with Cocobits")
+console.log("github.com/mileeme/cocobits | MIT License | v 1.0.0")
 
 
-// Change link color when active 
+// Define min-height of site wrapper for sticky footer
+const site = document.querySelector('main')
+const footer = document.querySelector('footer')
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log(window.location.pathname)
+if (footer) {
+    let footerHeight = footer.clientHeight
+    site.style.minHeight = "calc(100vh - " + footerHeight + "px)"
+}
+
+// Change main link color for selected page
+const urlPath = window.location.pathname.split('/')[1]
+const mainLinks = document.querySelectorAll('#sideNav a')
+
+mainLinks.forEach(link => {
+    const linkUrl = link.getAttribute('href').split('/')[1]
+    if (urlPath == linkUrl) {
+        link.classList.add('color-text-link')
+    }
 })
+
+
