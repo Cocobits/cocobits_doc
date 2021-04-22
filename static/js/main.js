a811ff03f9ce1ac530aef9d315cd48c50fdf441c
milee yu <mileeyu@gmail.com>
2021-04-22 00:15:51 -0700

    Replace minified js to js

--- static/js/main.js
+++ static/js/main.js
@@ -35,5 +35,364 @@ dropdownButton.addEventListener('click', (e) => {
     }
 })
 
-/*! COCOBITS JS | https://cocobits.github.io */
-function tab(){const e=document.querySelector(".tab-menu").children,t=document.querySelector(".tab-contents").children;e[0].classList.add("content--visible"),t[0].classList.add("d-block"),Array.from(t).forEach(e=>{e.classList.contains("d-block")||e.classList.add("d-none")}),Array.from(e).forEach(n=>{n.addEventListener("click",s=>{Array.from(e).forEach(e=>{e.classList.remove("content--visible")}),s.target===n&&n.classList.add("content--visible"),Array.from(t).forEach(e=>{n.dataset.tab===e.dataset.tab?e.classList.replace("d-none","d-block"):e.classList.replace("d-block","d-none")})})})}function accordion(){const e=document.querySelectorAll(".accordion");e.forEach(e=>{e.addEventListener("click",function(){e.classList.toggle("accordion-active"),e.classList.contains("accordion-active")?e.querySelector(".plus").style.setProperty("--plus-after","90deg"):e.querySelector(".plus").style.setProperty("--plus-after","0deg");let t=e.nextElementSibling;t.style.maxHeight?(t.style.maxHeight=null,t.style.padding="0px 16px"):(t.style.maxHeight=`${t.scrollHeight+48}px`,t.style.padding="24px 16px")})})}function carousel(){let e=document.querySelector(".carousel"),t=document.querySelector(".slide-container"),n=document.querySelectorAll(".slide"),s=n.length,l=e.offsetWidth,o=t.offsetHeight,a=s-1,[c,r]=[0,0],i=0,d=0,u=!1,y=document.querySelectorAll(".slide-nav"),m=document.querySelector(".slide-prev"),f=document.querySelector(".slide-next");function p(e){e.preventDefault(),"touchstart"==e.type?c=e.touches[0].clientX:"mousedown"==e.type&&(u=!0,c=e.clientX,t.onmousemove=v,t.onmouseup=L)}function v(e){e.preventDefault(),"touchmove"==e.type?(r=e.touches[0].clientX,i=d*l+(c-r),t.style.transform=`translateX(-${i}px)`):u&&(r=e.clientX,i=d*l+(c-r))}function L(e){e.preventDefault();let n=Math.abs(d*l-i);if("mouseup"!=e.type&&"touchend"!=e.type||n>50&&(c>r&&d<a?d++:c<r&&d>0&&d--),u=!1,t.classList.add("animate-transition"),t.style.transform=`translateX(-${d*l}px)`,y[0]){E(null,d)}}function h(e,n){if(e.preventDefault(),"click"==e.type&&("next"==n&&d<a?d++:"prev"==n&&d>0&&d--),t.classList.add("animate-transition"),t.style.transform=`translateX(-${d*l}px)`,y[0]){E(null,d)}}function E(e,n){null!=e&&(d=e,t.classList.add("animate-transition"),t.style.transform=`translateX(-${d*l}px)`,y.forEach(e=>{e.classList.remove("slide-nav--active")}),y[e].classList.add("slide-nav--active")),null!=n&&(y.forEach(e=>{e.classList.remove("slide-nav--active")}),y[n].classList.add("slide-nav--active"))}function b(e){e.preventDefault(),0==d?(m.style.display="none",f.style.display="block"):d==a?(m.style.display="block",f.style.display="none"):(m.style.display="block",f.style.display="block")}n[0]&&0===o&&requestAnimationFrame(carousel),n.forEach(e=>{e.style.width=`${l}px`}),t.style.width=`${s*l}px`,t.style.height=`${o}px`,t.classList.remove("animate-transition"),t.style.transform="translateX(0px)",y[0]&&y[0].classList.add("slide-nav--active"),m&&f&&(m.style.setProperty("--slide-controller-center",`${o/2}px`),f.style.setProperty("--slide-controller-center",`${o/2}px`),1==s?(m.style.display="none",f.style.display="none"):(m.style.display="none",f.style.display="block")),t.onmousedown=p,t.addEventListener("touchstart",p,!1),t.addEventListener("touchmove",v,!1),t.addEventListener("touchend",L,!1),t.addEventListener("transitionend",b,!1),(m&&f||y[0]&&m&&f)&&(m.addEventListener("click",e=>{h(e,"prev")},!1),f.addEventListener("click",e=>{h(e,"next")},!1)),y[0]&&y.forEach((e,t)=>{let n=t;e.addEventListener("click",e=>{E(n,null),m&&f&&b(e)},!1)})}function scrollX(){const e=document.querySelector(".scrollX"),t=e.offsetLeft;let n,s,l,o,a,c,r=!1;function i(){cancelAnimationFrame(c)}function d(){e.scrollLeft+=a,a*=.95,Math.abs(a)>.75&&(c=requestAnimationFrame(d))}e.addEventListener("mousedown",function(l){l.preventDefault(),r=!0,e.classList.add("scrollX--active"),n=l.clientX-t,s=e.scrollLeft,i()}),e.addEventListener("mousemove",function(c){if(!r)return;c.preventDefault(),l=c.clientX-t,o=l-n;let i=s;e.scrollLeft=s-o,a=e.scrollLeft-i}),e.addEventListener("mouseup",()=>{r=!1,e.classList.remove("scrollX--active"),i(),c=requestAnimationFrame(d)}),e.addEventListener("mouseleave",()=>{r=!1,e.classList.remove("scrollX--active"),e.querySelectorAll("a")&&e.querySelectorAll("a").forEach(e=>{e.classList.remove("disable-pointer")})}),e.addEventListener("wheel",e=>{i()})}document.addEventListener("DOMContentLoaded",function(){document.querySelector(".tab-menu")&&tab(),document.querySelector(".accordion")&&accordion(),document.querySelector(".slide-container")&&carousel(),document.querySelector(".scrollX")&&scrollX()}),window.addEventListener("resize",function(){carousel()});
+/* COCOBITS JS | https://cocobits.github.io | v2.6.7 */
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelector('.tab-menu')) { tab() }
+  if (document.querySelector('.accordion')) { accordion() }
+  if (document.querySelector('.slide-container')) { carousel() }
+  if (document.querySelector('.scrollX')) { scrollX() }
+})
+
+// Resize carousel slides for each window resize
+window.addEventListener('resize', function () {
+  carousel()
+})
+
+/* TAB */
+function tab() {
+  // Initialize variables
+  const tabLinks = document.querySelector('.tab-menu').children,
+        tabContents = document.querySelector('.tab-contents').children
+
+
+  // Select first tab link by default
+  tabLinks[0].classList.add('content--visible')
+  // Show first tab content by default
+  tabContents[0].classList.add('d-block')
+  // Hide rest of content by default
+  Array.from(tabContents).forEach(child => {
+    if (child.classList.contains('d-block')) {
+      return
+    } else {
+      child.classList.add('d-none')
+    }
+  })
+
+  Array.from(tabLinks).forEach(link => {
+    
+    link.addEventListener('click', (e) => {
+      Array.from(tabLinks).forEach(link => {
+        link.classList.remove('content--visible')
+      })
+      // Change color of selected link
+      if (e.target === link) {
+        link.classList.add('content--visible')
+      }
+      // Show content of selected link
+      Array.from(tabContents).forEach(content => {
+        if (link.dataset.tab === content.dataset.tab) {
+          content.classList.replace('d-none', 'd-block')
+        } else {
+          content.classList.replace('d-block', 'd-none')
+        }
+      })
+    })
+  })
+}
+
+/* Accordion */
+function accordion() {
+  const accButtons = document.querySelectorAll('.accordion'),
+    py = 48
+
+  // Listen for click
+  accButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      button.classList.toggle('accordion-active')
+
+      if (button.classList.contains('accordion-active')) {
+        button.querySelector('.plus').style.setProperty('--plus-after', '90deg')
+      } else {
+        button.querySelector('.plus').style.setProperty('--plus-after', '0deg')
+      }
+      let content = button.nextElementSibling
+
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null
+        content.style.padding = '0px 16px'
+      } else {
+        content.style.maxHeight = `${content.scrollHeight + py}px`
+        content.style.padding = '24px 16px'
+      }
+    })
+  })
+}
+
+/* CAROUSEL */
+function carousel() {
+  // Initialize variables
+  let carouselContainer = document.querySelector('.carousel'),
+    slideContainer = document.querySelector('.slide-container'),
+    slides = document.querySelectorAll('.slide'),
+    slidesCount = slides.length,
+    slideWidth = carouselContainer.offsetWidth,
+    slideHeight = slideContainer.offsetHeight,
+    lastIndex = slidesCount - 1,
+    [startX, moveX] = [0, 0],
+    distanceX = 0,
+    index = 0,
+    mouseMoving = false,
+    navButtons = document.querySelectorAll('.slide-nav'),
+    prev = document.querySelector('.slide-prev'),
+    next = document.querySelector('.slide-next')
+
+  if (slides[0] && slideHeight === 0) {
+    requestAnimationFrame(carousel)
+  }
+
+  console.log(slideHeight)
+
+  // Set width of each slide
+  slides.forEach(slide => {
+    slide.style.width = `${slideWidth}px`
+  })
+
+  // Set the slide container height and width
+  slideContainer.style.width = `${slidesCount * slideWidth}px`
+  slideContainer.style.height = `${slideHeight}px`
+
+  // Reset slide container on page load or window resize
+  slideContainer.classList.remove('animate-transition')
+  slideContainer.style.transform = 'translateX(0px)'
+
+  // Style navigation items if available
+  if (navButtons[0]) { navButtons[0].classList.add('slide-nav--active') }
+  if (prev && next) {
+    prev.style.setProperty('--slide-controller-center', `${slideHeight / 2}px`)
+    next.style.setProperty('--slide-controller-center', `${slideHeight / 2}px`)
+    if (slidesCount == 1) {
+      prev.style.display = 'none'
+      next.style.display = 'none'
+    } else {
+      prev.style.display = 'none'
+      next.style.display = 'block'
+    }
+  }
+
+  // EventListeners
+  slideContainer.onmousedown = start
+  slideContainer.addEventListener('touchstart', start, false)
+  slideContainer.addEventListener('touchmove', move, false)
+  slideContainer.addEventListener('touchend', end, false)
+  slideContainer.addEventListener('transitionend', transition, false)
+
+  if (prev && next || navButtons[0] && (prev && next)) {
+    prev.addEventListener('click', (event) => { shiftSlide(event, 'prev') }, false)
+    next.addEventListener('click', (event) => { shiftSlide(event, 'next') }, false)
+  }
+  if (navButtons[0]) {
+    navButtons.forEach((nav, i) => {
+      let clickIndex = i
+      nav.addEventListener('click', (event) => {
+        navigateToSlide(clickIndex, null)
+        if (prev && next) { transition(event) }
+      }, false)
+    })
+  }
+
+  // Start function
+  function start(event) {
+    event.preventDefault()
+
+    if (event.type == 'touchstart') {
+      // Get initial touch position
+      startX = event.touches[0].clientX
+    } else if (event.type == 'mousedown') {
+      mouseMoving = true
+      startX = event.clientX
+      slideContainer.onmousemove = move
+      slideContainer.onmouseup = end
+    }
+  }
+
+  // Move function
+  function move(event) {
+    event.preventDefault()
+
+    if (event.type == 'touchmove') {
+      // Get moving touch position
+      moveX = event.touches[0].clientX
+      // Calc container touch movement
+      distanceX = (index * slideWidth) + (startX - moveX)
+      // Limit container from moving beyond last slide
+      slideContainer.style.transform = `translateX(-${distanceX}px)`
+    } else if (mouseMoving) {
+      moveX = event.clientX
+      // Calc container mouse movement
+      distanceX = (index * slideWidth) + (startX - moveX)
+    }
+  }
+
+  // End function
+  function end(event) {
+    event.preventDefault()
+
+    // Calc distance swiped 
+    let absMove = Math.abs(index * slideWidth - distanceX)
+
+    if (event.type == 'mouseup' || event.type == 'touchend') {
+      if (absMove > 50) {
+        if (startX > moveX && index < lastIndex) {
+          index++
+        } else if (startX < moveX && index > 0) {
+          index--
+        }
+      }
+    }
+    mouseMoving = false
+    // Move and animate container
+    slideContainer.classList.add('animate-transition')
+    slideContainer.style.transform = `translateX(-${index * slideWidth}px)`
+
+    // Move to new nav position
+    if (navButtons[0]) {
+      let slideIndex = index
+      navigateToSlide(null, slideIndex)
+    }
+  }
+
+  // Shiftslide function
+  function shiftSlide(event, clickEvent) {
+    event.preventDefault()
+
+    if (event.type == 'click') {
+      if (clickEvent == 'next' && index < lastIndex) {
+        index++
+      } else if (clickEvent == 'prev' && index > 0) {
+        index--
+      }
+    }
+    // Move and animate container
+    slideContainer.classList.add('animate-transition')
+    slideContainer.style.transform = `translateX(-${index * slideWidth}px)`
+
+    // Move to new nav position
+    if (navButtons[0]) {
+      let slideIndex = index
+      navigateToSlide(null, slideIndex)
+    }
+  }
+
+  // Navigate to slide function
+  function navigateToSlide(clickIndex, slideIndex) {
+    if (clickIndex != null) {
+      index = clickIndex
+      // Move and animate container
+      slideContainer.classList.add('animate-transition')
+      slideContainer.style.transform = `translateX(-${index * slideWidth}px)`
+      // Move to new nav position
+      navButtons.forEach(nav => {
+        nav.classList.remove('slide-nav--active')
+      })
+      navButtons[clickIndex].classList.add('slide-nav--active')
+    }
+
+    // Move to new nav position
+    if (slideIndex != null) {
+      navButtons.forEach(nav => {
+        nav.classList.remove('slide-nav--active')
+      })
+      navButtons[slideIndex].classList.add('slide-nav--active')
+    }
+  }
+
+  // Transition function
+  function transition(event) {
+    event.preventDefault()
+
+    // Show and hide arrow controller based on slide index position
+    if (index == 0) {
+      prev.style.display = 'none'
+      next.style.display = 'block'
+    } else if (index == lastIndex) {
+      prev.style.display = 'block'
+      next.style.display = 'none'
+    } else {
+      prev.style.display = 'block'
+      next.style.display = 'block'
+    }
+  }
+}
+
+/*! SCROLLX */
+function scrollX() {
+  const container = document.querySelector('.scrollX'),
+      containerLeft = container.offsetLeft
+  
+  let scrollXActive = false,
+      startX,
+      scrollLeft,
+      distanceX,
+      moveX,
+      velX,
+      momentumID
+
+  container.addEventListener('mousedown', start) 
+  container.addEventListener('mousemove', move)
+
+  container.addEventListener('mouseup', () => {
+    scrollXActive = false
+    container.classList.remove('scrollX--active')
+    momentumStart()
+  })
+  container.addEventListener('mouseleave', () => {
+    scrollXActive = false
+    container.classList.remove('scrollX--active')
+    // Disable anchors
+    if (container.querySelectorAll('a')) {
+      container.querySelectorAll('a').forEach(link => {
+        link.classList.remove('disable-pointer')
+      })
+    }
+  })
+  container.addEventListener('wheel', (event) => {
+    momentumEnd()
+  })
+
+  // Start 
+  function start(event) {
+    event.preventDefault()
+    // Allow mouse click and drag
+    scrollXActive = true
+    container.classList.add('scrollX--active')
+
+    // Get mousedown position
+    startX = event.clientX - containerLeft
+    scrollLeft = container.scrollLeft
+    momentumEnd()
+  }
+  
+  // Move
+  function move(event) {
+    if (!scrollXActive) return 
+    event.preventDefault()
+
+    distanceX = event.clientX - containerLeft
+    moveX = (distanceX - startX)
+
+    let prevScrollLeft = scrollLeft
+
+    container.scrollLeft = scrollLeft - moveX
+    velX = container.scrollLeft - prevScrollLeft
+  }
+
+  // Momentum
+  function momentumStart() {
+    momentumEnd()
+    momentumID = requestAnimationFrame(momentumLoop)
+  }
+
+  function momentumEnd() {
+    cancelAnimationFrame(momentumID)
+  }
+
+  function momentumLoop() {
+    container.scrollLeft += velX 
+    velX *= 0.95
+
+    if (Math.abs(velX) > 0.75) {
+      momentumID = requestAnimationFrame(momentumLoop)
+    }
+  }
+}
